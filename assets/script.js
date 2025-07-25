ba4b189c3f1da6d586247b670d82ec852b9ec137
FrenchHelp <Ryanc6145@gmail.com>
2025-07-25 15:57:29 -0400

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+//nav and footer loading script
+$(function () {
+  $("#nav-placeholder").load("nav.html");
+  $("#footer-placeholder").load("footer.html");
+  $("#not-logged-in").load("404.html");
+});
+
